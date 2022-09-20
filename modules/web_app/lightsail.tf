0b456228dee5774fed6939ec73b19492c4985b19
Andrei Ioniță <hi@andrei.io>
2022-09-20 17:36:27 +0100

    simplify certificate name

--- modules/web_app/lightsail.tf
+++ modules/web_app/lightsail.tf
@@ -8,7 +8,7 @@ resource "aws_lightsail_container_service" "container_service" {
 
     content {
       certificate {
-        certificate_name = "${public_domain_names.value}-certificate"
+        certificate_name = public_domain_names.value
         domain_names = [
           public_domain_names.value,
           "www.${public_domain_names.value}"
