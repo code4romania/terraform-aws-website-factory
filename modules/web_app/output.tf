47d5becf127e6accb321f4483c471eb17c7bb2ee
Andrei Ioniță <hi@andrei.io>
2022-09-20 17:42:59 +0100

    Lightsail improvements (#1)
    * add lightsail public_domain_names
    
    * fix dynamic block content
    
    * simplify certificate name
    
    * output public url

--- modules/web_app/output.tf
+++ modules/web_app/output.tf
@@ -1,3 +1,7 @@
+output "web_app_hostname" {
+  value = aws_lightsail_container_service.container_service.url
+}
+
 output "admin_password" {
   value     = random_password.admin_password.result
   sensitive = true
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "web_app_hostname" {
+  value = module.web_app.web_app_hostname
+}
+
 output "admin_password" {
   value     = module.web_app.admin_password
   sensitive = true
